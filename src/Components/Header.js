3a79cc9163267cad5f3f2b410e59878800cce4b5
Guy Nahon <guy.nahon@gmail.com>
2023-12-19 22:03:09 +0200

    header component content now changes when route is changing

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,42 @@
 import './Header.css'
+import {useLocation, useParams} from "react-router-dom";
+import {useContext, useEffect, useState} from "react";
+import {BlogContext} from "../Providers/BlogProvider";
 export function Header () {
+    const {posts} = useContext(BlogContext);
+    const [pageHeader, setPageHeader] = useState(null);
+    const [pageParagraph, setPageParagraph] = useState(null);
+    const {id} = useParams();
+
+    console.log(posts);
+    const location = useLocation().pathname;
+
+    useEffect(() => {
+        switch (location) {
+            case "/":
+                setPageHeader("Welcome to Guy's blog!");
+                setPageParagraph("The only place you will find happiness!");
+                break;
+
+            case "/subject":
+                setPageHeader("topic 123");
+                setPageParagraph("a very nice paragraph!");
+                break;
+
+            case `/article/${id}`:
+                const post = posts.find(p => p.id === Number(id));
+                if (post) {
+                    setPageHeader(post.title);
+                    setPageParagraph(null);
+                }
+                break;
+        }
+    }, [location, posts]);
+
     return (
         <div className="main-header">
-            <div className="page-header">Minimal blog template for creative expressions</div>
-            <p className="page-paragraph">100% customisable and SEO-friendly blog template for personal and commercial purposes.</p>
+            <div className="page-header">{pageHeader}</div>
+            {pageParagraph && <p className="page-paragraph">{pageParagraph}</p>}
         </div>
     );
 }
--- src/Providers/BlogProvider.js
+++ src/Providers/BlogProvider.js
@@ -6,57 +6,45 @@ export function BlogProvider({children}) {
     const [posts, setPosts] = useState([]);
     const [selectedPost, setSelectedPost] = useState(null);
 
- // useEffect(() => {
- //     fetch("https://jsonplaceholder.typicode.com/posts").then(response => response.json())
- //         .then(json => setPosts(json));
- // }, []);
+    const addPost = (singlePost) => {
+        setPosts([...posts, singlePost]);
+        localStorage.setItem("posts", JSON.stringify([...posts, singlePost]));
+    };
 
-// useEffect(() => {
-//     localStorage.setItem("posts", JSON.stringify(posts));
-// }, [posts]);
+    const removePost = (postId) => {
+        setPosts(posts.filter(post => post.id !== postId));
+        localStorage.setItem("posts", JSON.stringify(posts.filter(post => post.id !== postId)));
+    };
 
- const addPost = (singlePost) => {
-     setPosts([...posts, singlePost]);
-     localStorage.setItem("posts", JSON.stringify([...posts, singlePost]));
- };
+    const clearPosts = () => {
+        setPosts([]);
+        localStorage.setItem("posts", []);
+    };
 
-const removePost = (postId) => {
-    setPosts(posts.filter(post => post.id !== postId));
-    localStorage.setItem("posts", JSON.stringify(posts.filter(post => post.id !== postId)));
-};
+    const editPost = (post) => {
+        setSelectedPost(post);
+    }
 
- const clearPosts = () => {
-     setPosts([]);
-     localStorage.setItem("posts", []);
- };
+    const updatePost = (selectedPost, data) => { //after edit
+        const arr = [...posts];
+        const i = posts.findIndex(post => post.id === selectedPost.id);
+        arr[i].title = data.title;
+        arr[i].body = data.body;
+        arr[i].date = data.createdAt;
+        setPosts(arr);
+        localStorage.setItem("posts", JSON.stringify(arr));
+    }
 
- const editPost = (post) => {
-     setSelectedPost(post);
-     // console.log(posts.map((p) => (p.id === post.id ? post : p)));
-     // localStorage.setItem("posts", JSON.stringify(posts.map((p) => (p.id === post.id ? post : p))));
- }
-
- const updatePost = (selectedPost, data) => {
-     const arr = [...posts];
-     const i = posts.findIndex(post => post.id === selectedPost.id);
-     arr[i].title = data.title;
-     arr[i].body = data.body;
-     arr[i].date = data.createdAt;
-     setPosts(arr);
-     localStorage.setItem("posts", JSON.stringify(arr));
- }
-
-useEffect(() => {
-    const postsString = localStorage.getItem("posts");
-    setPosts(JSON.parse(postsString));
-}, []);
+    useEffect(() => {
+        const postsString = localStorage.getItem("posts");
+        setPosts(JSON.parse(postsString));
+    }, []);
 
 
- const value = {posts, selectedPost, setSelectedPost, addPost, removePost, clearPosts, editPost, updatePost}
- return (
-     <BlogContext.Provider value = {value}>
-         {children}
-     </BlogContext.Provider>
-
- );
+    const value = {posts, selectedPost, setSelectedPost, addPost, removePost, clearPosts, editPost, updatePost}
+    return (
+        <BlogContext.Provider value = {value}>
+            {children}
+        </BlogContext.Provider>
+    );
 }
