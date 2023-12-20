128af675d6499513eedd139a734139a1ebb8393b
Guy Nahon <guy.nahon@gmail.com>
2023-12-20 17:47:56 +0200

    added comments in some files

--- src/CardComponents/ArticleCard.js
+++ src/CardComponents/ArticleCard.js
@@ -5,22 +5,37 @@ import {BlogContext} from "../Providers/BlogProvider";
 import {AuthContext} from "../Providers/AuthProvider";
 import {scrollToTop} from "../helper-functions/scrollToTop";
 
+
+// gets a singlePost param from its father component and displays that posts as a card in the ui
 export function ArticleCard({singlePost}) {
+
+    // to use removePost function later we assign the post id to a const id
     const id = singlePost.id;
+
+    // we will use this route to link us to the cards related article link
     const route = `/article/${id}`;
+
+    // getting the removePost and editPost functions so we can click on button/link and we will execute them.
     const {removePost, editPost} = useContext(BlogContext);
+
+    // getting the user value to check if we are signed in, if we aren't signed in we won't be able to see
+    // the edit-post/remove-post link/button
     const {user} = useContext(AuthContext);
 
+    // parsing the date in the way we want to display it
     const currentDate = singlePost.date.split("-");
     const day = currentDate[2];
     const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     const month = monthNames[Number(currentDate[1]-1)];
     const year = currentDate[0];
+    const cardDate = `${day} ${month} ${year}`;
 
     const handlePostEdit = () => {
         editPost(singlePost);
     }
 
+    // when we press the edit-post link this function is called, it calls the handlePostEdit that calls the
+    // editPost with the singlePost to edit, and scroll to top which scroll the page back up
     const handlePostEditClick = () => {
         handlePostEdit();
         scrollToTop();
@@ -29,25 +44,28 @@ export function ArticleCard({singlePost}) {
     return (
             <div className="card">
                 <div className="card-content-frame">
-                    <h5 className="card-date">{day+" "+month+" "+year}</h5>
+                    <h5 className="card-date">{cardDate}</h5>
                     <div className="card-text">
                         <h3 className="card-header">{singlePost.title}</h3>
                         <p className="card-paragraph">{singlePost.body}</p>
                     </div>
                     <div className="card-footer">
                         <div className="read-more" onClick={scrollToTop}><NavLink to={route}>Read More</NavLink></div>
+                        {/*displays the edit and remove only if user logged in*/}
                         {user && (
                             <>
                                 <div className="card-edit-btn">
                                     <NavLink to="/admin" onClick={handlePostEditClick}>Edit Post</NavLink>
                                 </div>
                                 <div className="card-remove-btn">
+                                    {/*calling the remove function onClick with the posts id*/}
                                     <button onClick={() => removePost(id)}>X</button>
                                 </div>
                             </>
                         )}
                     </div>
                 </div>
+                {/*an image tag with a src that is api that displays random images*/}
                 <img src={`https://picsum.photos/300/300?${singlePost.id}`} alt="random-picture"/>
             </div>
     );
