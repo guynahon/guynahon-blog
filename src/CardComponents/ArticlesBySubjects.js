d0899e8092eb4c9218960982b86b7bfa511cc8a5
Guy Nahon <guy.nahon@gmail.com>
2023-12-20 17:32:51 +0200

    added comments in some files

--- src/CardComponents/ArticlesBySubjects.js
+++ src/CardComponents/ArticlesBySubjects.js
@@ -1,6 +1,7 @@
 import {ThreeCardsList} from "./ThreeCardsList";
 import './ArticlesBySubjects.css'
 
+// this component helps us get 3 article cards from each subject (3 in total) and display them
 export function ArticlesBySubjects() {
     return (
         <div id="articles">
--- src/CardComponents/ThreeCardsList.js
+++ src/CardComponents/ThreeCardsList.js
@@ -5,14 +5,21 @@ import {useContext, useEffect, useState} from "react";
 import {BlogContext} from "../Providers/BlogProvider";
 import {filterPostsBySubject} from "../helper-functions/filterPostsBySubject";
 
+// this component gets the subject as param from its father component and based on it
 export function ThreeCardsList({subject}) {
+    // posts array from the BlogContext
     const {posts} = useContext(BlogContext);
     const [threePosts, setThreePosts] = useState([]);
 
+    // we run the function if the posts changed, because we are getting them from the local storage,
+    // the small delay can get us an empty array, so we track the change in posts running the function inside
+    // the useEffect on every change. the function inside return an array of posts filtered by subject
+    // and sets its value to the threePost useState.
     useEffect(() => {
         setThreePosts(filterPostsBySubject(subject, posts).slice(0,3));
     }, [posts]);
 
+    // an object that help us set the article-section-header based on the subject we got as param
     const articleHeaders = {
         "dailydigest": "Daily Digest",
         "designtools": "Design Tools",
@@ -25,6 +32,8 @@ export function ThreeCardsList({subject}) {
                 <h3 className="article-section-header">{articleHeaders[subject]}</h3>
                 <ViewAllButton subject={subject}/>
             </div>
+            {/* to display each post in the threePosts, we map each post to an ArticleCard component and sending
+             the post as "singlePost" param*/}
             {threePosts.map((post) => <ArticleCard singlePost={post} />)}
         </div>
     );
