15c32bca00bf53f8ee4d7ddcf5074b1c63ea6716
Guy Nahon <guy.nahon@gmail.com>
2023-12-27 19:59:29 +0200

    added darkmode to all except admin

--- src/EveryPageComponents/NewsLetter.js
+++ src/EveryPageComponents/NewsLetter.js
@@ -1,15 +1,20 @@
 import './NewsLetter.css'
+import {useContext} from "react";
+import {DarkContext} from "../Providers/DarkProvider";
 export function NewsLetter() {
+
+    const {isDarkMode} = useContext(DarkContext);
+
     // static newsletter component
     return (
-        <div className="news-letter">
-            <div className="news-letter-text">
-                <h3 className="news-letter-header">Guy's Blog Newsletter</h3>
-                <p className="news-letter-paragraph">A bi-weekly newsletter of design inspiration, resources and anything related to career development.</p>
+        <div className={`news-letter ${isDarkMode ? "dark" : ""}`}>
+            <div className={`news-letter-text ${isDarkMode ? "dark" : ""}`}>
+                <h3 className={`news-letter-header ${isDarkMode ? "dark" : ""}`}>Guy's Blog Newsletter</h3>
+                <p className={`news-letter-paragraph ${isDarkMode ? "dark" : ""}`}>A bi-weekly newsletter of design inspiration, resources and anything related to career development.</p>
             </div>
-            <div className="news-letter-email-box">
-                <input className="news-letter-input" type="text" placeholder="Email Address"/>
-                <a className="news-letter-button">Subscribe</a>
+            <div className={`news-letter-email-box ${isDarkMode ? "dark" : ""}`}>
+                <input className={`news-letter-input ${isDarkMode ? "dark" : ""}`} type="text" placeholder="Email Address"/>
+                <a className={`news-letter-button ${isDarkMode ? "dark" : ""}`}>Subscribe</a>
             </div>
         </div>
     );
